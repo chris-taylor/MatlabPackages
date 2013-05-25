691afdd45c270af2948cc87b4855803cbac993b7
Chris Taylor <crntaylor@gmail.com>
2013-05-25 01:40:25 +0100

    Tidied up a bit, added toString method

--- +containers/@Set/Set.m
+++ +containers/@Set/Set.m
@@ -1,63 +1,62 @@
 classdef Set < containers.Container
-% SET A mathematical set, backed by a Java HashSet.
+% SET A mathematical set, backed by a Java HashSet. It implements the
+% Container abstract class.
 
     properties (Access = private)
-        set_
+        set
     end
     
     methods (Access = public)
        
         function obj = Set(varargin)
-            obj.set_ = java.util.HashSet;
+            obj.set = java.util.HashSet;
             for i = 1:length(varargin)
-                obj.set_.add(varargin{i});
+                obj.set.add(varargin{i});
             end
         end
         
+        %========================
+        % Container methods
+        %========================
+        
         function res = contains(obj,x)
-            res = obj.set_.contains(x);
+            res = obj.set.contains(x);
         end
         
         function add(obj,varargin)
             for i = 1:length(varargin)
-                obj.set_.add(varargin{i});
+                obj.set.add(varargin{i});
             end
         end
         
         function clear(obj)
-            obj.set_.clear();
+            obj.set.clear();
         end
         
         function res = isEmpty(obj)
-            res = obj.set_.isEmpty();
+            res = obj.set.isEmpty();
         end
         
         function remove(obj,varargin)
             for i = 1:length(varargin)
-                obj.set_.remove(varargin{i});
+                obj.set.remove(varargin{i});
             end
         end
         
         function res = size(obj)
-            res = obj.set_.size();
-        end
-        
-        function res = values(obj)
-            res = cell(1,obj.size);
-            itr = obj.set_.iterator();
-            n = 0;
-            while itr.hasNext();
-                n = n + 1;
-                res{n} = itr.next();
-            end
+            res = obj.set.size();
         end
         
         function itr = iterator(obj)
-            itr = obj.set_.iterator();
+            itr = obj.set.iterator();
         end
         
-        function disp(obj)
-            disp(obj.set_.toString());
+        %========================
+        % Convenience methods
+        %========================
+        
+        function str = toString(obj)
+            str = char(obj.set.toString);
         end
         
     end
